d077a33de38b5141d73a13182fd4ff0356c7717b
Bhupesh Dhesingurajan <rajanbhupesh@gmail.com>
2025-03-28 11:43:09 +0530

    react-indicator and throw block created

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,57 +1,91 @@
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import {Mosaic} from 'react-loading-indicators'
 
 const ProductList = () => {
 
   let [products,setProducts] = useState([]);
+  let [error,setError] = useState("");
+  let [loading,isLoading] = useState(true);
 
   useEffect( ()=>{
     console.log("useEffect running...");
-    fetch( "http://localhost:5000/products", { method:"GET" } )
-    .then( (response)=>{ return response.json()})
-
-    .then( (data)=>{ setProducts(data) } )
+    fetch( "http://localhost:5000/products/", { method:"GET" } )
+    .then( (response)=>{ 
+      if(response.ok){
+        return response.json();
+      }
+      else{
+        throw new Error( "search Proper Data" );
+      }
+    })
+    .then( (data)=>{ 
+      setProducts(data) 
+    })
+    .catch( (error)=>{ 
+      setError(error.message) 
+    })
+    .finally( ()=>{ 
+      isLoading(false) 
+    })
   },[]);
 
-  return (
-    <div>
-      <h1 style={ { textAlign:'center',textDecoration:'underline',textTransform:'uppercase', fontFamily:'cursive sans-serif' } }>Product List</h1>
-      <section className='products'>
+  if(loading){
+    return (
+    <center>
+      <div style={ { marginTop:"200px" } }>
+        <Mosaic color="#6a31cc" size="large" text="loading" textColor="#5c28b6" />
+      </div>
+    </center>
+    )}
+  else{
+    return (
+      <div>
+        <h1 style={ { textAlign:'center',textDecoration:'underline',textTransform:'uppercase', fontFamily:'cursive sans-serif' } }>Product List</h1>
         {
-          products.map( (product)=>{
-            return(
-              <Card style={{ width: '18rem' }} className='product'>
-             
-                <Card.Img variant="top" 
-                src= { product.image } 
-                style={ { width:"9rem", height :"10rem",margin:"auto",marginTop:"25px"} }
-                />
-                <Card.Body>
-                  <Card.Title>{ product.title }</Card.Title>
-                  <Card.Text style={ {overflow:"scroll",height:"200px", textAlign:"justify"} }>
-                    { product.description }
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer 
-                style={ { 
-                  display:"flex",
-                  justifyContent:"space-evenly",
-                  alignItems:"center" }}
-                >
-                  <Card.Text>${product.price} </Card.Text>
-                  <Button variant="primary">Add to Cart</Button>
-                </Card.Footer>
-              </Card>
-              
-
-            )
-         })
+           products.length !==0 && (
+            <section className='products'>
+          {
+            products.map( (product)=>{
+              return(
+                <Card key={ product.id } style={{ width: '18rem' }} className='product'>
+               
+                  <Card.Img variant="top" 
+                  src= { product.image } 
+                  style={ { width:"9rem", height :"10rem",margin:"auto",marginTop:"25px"} }
+                  />
+                  <Card.Body>
+                    <Card.Title>{ product.title }</Card.Title>
+                    <Card.Text style={ {overflow:"scroll",height:"200px", textAlign:"justify"} }>
+                      { product.description }
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer 
+                  style={ { 
+                    display:"flex",
+                    justifyContent:"space-evenly",
+                    alignItems:"center" }}
+                  >
+                    <Card.Text>${product.price} </Card.Text>
+                    <Button variant="primary">Add to Cart</Button>
+                  </Card.Footer>
+                </Card>
+                
+  
+              )
+           })
+          }
+        </section>
+           )
         }
-      </section>
-    </div>
-    
-  )
+        
+        {
+          error && <p> {error} </p>
+        }
+      </div>  
+    )
+  }
 }
 
 export default ProductList;
